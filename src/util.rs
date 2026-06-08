ecd9c28b3515f3efddfa676e8d95d8484032243f
Naz <ndpm13@ch-naseem.com>
2026-06-08 17:03:45 +0100

    feat: add in-app log view for script execution
    - Wrap main content in a GTK Stack with "main" and "log" pages
    - Add log page with a scrollable TextView to stream script output
      in-app instead of spawning a separate Alacritty terminal window
    - Run system_update.sh via pkexec + gio::Subprocess with STDOUT_PIPE
      and STDERR_MERGE so output is captured and displayed in the log view

--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,9 @@
  * Copyright (C) 2026 Naz <ndpm13@ch-naseem.com>
  */
 
+use gio::prelude::*;
+use gtk::prelude::*;
+
 use crate::config::SCRIPTSDIR;
 
 pub fn autostart_file() -> std::path::PathBuf {
@@ -14,4 +17,29 @@ pub fn autostart_file() -> std::path::PathBuf {
 
 pub fn scripts_dir() -> std::path::PathBuf {
     std::path::PathBuf::from(SCRIPTSDIR)
+}
+
+pub fn read_line_utf8_async_to_buffer<F>(
+    stream: gio::DataInputStream,
+    buffer: gtk::TextBuffer,
+    on_done: F,
+) where
+    F: Fn() + 'static,
+{
+    stream.read_line_utf8_async(
+        gio::glib::Priority::DEFAULT,
+        gio::Cancellable::NONE,
+        gio::glib::clone!(
+            #[strong]
+            stream,
+            move |result| {
+                if let Ok(Some(line)) = result {
+                    buffer.insert(&mut buffer.end_iter(), &format!("{line}\n"));
+                    read_line_utf8_async_to_buffer(stream, buffer, on_done);
+                } else {
+                    on_done();
+                }
+            }
+        ),
+    );
 }
--- src/window/imp.rs
+++ src/window/imp.rs
@@ -3,12 +3,18 @@
  * Copyright (C) 2026 Naz <ndpm13@ch-naseem.com>
  */
 
+use std::ffi::OsStr;
+
 use super::*;
-use crate::util::{autostart_file, scripts_dir};
+use crate::util::{autostart_file, read_line_utf8_async_to_buffer, scripts_dir};
 
 #[derive(Debug, Default, gtk::CompositeTemplate)]
 #[template(resource = "/com/ch-naseem/NoidWelcome/ui/window.ui")]
 pub struct NoidWelcomeWindow {
+    #[template_child]
+    pub stack: TemplateChild<gtk::Stack>,
+
+    // main stack
     #[template_child]
     pub switch_autostart: TemplateChild<gtk::Switch>,
 
@@ -20,10 +26,24 @@ pub struct NoidWelcomeWindow {
 
     #[template_child]
     pub button_oxidize_system: TemplateChild<gtk::Button>,
+
+    // log stack
+    #[template_child]
+    pub text_view_log: TemplateChild<gtk::TextView>,
+
+    #[template_child]
+    pub button_return: TemplateChild<gtk::Button>,
 }
 
 #[gtk::template_callbacks]
 impl NoidWelcomeWindow {
+    #[template_callback]
+    fn on_button_return_clicked(&self) {
+        self.stack.set_visible_child_name("main");
+        self.text_view_log.buffer().set_text("");
+        self.button_return.set_visible(false)
+    }
+
     #[template_callback]
     fn on_switch_autostart_state_set(&self, state: bool) -> glib::Propagation {
         let autostart_file = autostart_file();
@@ -43,9 +63,40 @@ impl NoidWelcomeWindow {
 
     #[template_callback]
     fn on_button_system_update_clicked(&self) {
-        let _ = std::process::Command::new(scripts_dir().join("system_update.sh"))
-            .spawn()
-            .expect("failed to update your system");
+        self.stack.set_visible_child_name("log");
+
+        let tweak = &scripts_dir()
+            .join("system_update.sh")
+            .to_string_lossy()
+            .to_string();
+
+        let argv = &[
+            OsStr::new("pkexec"),
+            OsStr::new("bash"),
+            OsStr::new("-c"),
+            OsStr::new(tweak),
+        ];
+
+        let subprocess = gio::Subprocess::newv(
+            argv,
+            gio::SubprocessFlags::STDOUT_PIPE.union(gio::SubprocessFlags::STDERR_MERGE),
+        )
+        .unwrap();
+        let stream = gio::DataInputStream::new(&subprocess.stdout_pipe().unwrap());
+
+        let buffer = self.text_view_log.buffer();
+
+        read_line_utf8_async_to_buffer(
+            stream,
+            buffer,
+            glib::clone!(
+                #[strong(rename_to = window)]
+                self.obj(),
+                move || {
+                    window.imp().button_return.set_visible(true);
+                }
+            ),
+        );
     }
 
     #[template_callback]
